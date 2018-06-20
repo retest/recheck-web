63418e61d86edc2ce68ffb46c253b0cf936a4099
Daniel Kraus <daniel.kraus@retest.de>
2018-06-20 15:38:01 +0200

    Port RecheckSeleniumAdapterTest to JUnit 5

--- src/test/java/de/retest/web/RecheckSeleniumAdapterTest.java
+++ src/test/java/de/retest/web/RecheckSeleniumAdapterTest.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import de.retest.elementcollection.ElementCollection;
 import de.retest.elementcollection.IgnoredComponents;
@@ -16,25 +16,25 @@ import de.retest.ui.descriptors.RootElement;
 import de.retest.ui.diff.RootElementDifference;
 import de.retest.ui.diff.RootElementDifferenceFinder;
 
-public class RecheckSeleniumAdapterTest {
+class RecheckSeleniumAdapterTest {
 
-	@Before
-	public void setUp() {
+	@BeforeEach
+	void setUp() {
 		IgnoredComponents.getTestInstance( new ElementCollection() );
 	}
 
 	@Test
-	public void getParentPath_should_return_null_for_toplevel() {
+	void getParentPath_should_return_null_for_toplevel() {
 		assertThat( RecheckSeleniumAdapter.getParentPath( "//HTML[1]" ) ).isNull();
 	}
 
 	@Test
-	public void getParentPath_should_return_parent_path() {
+	void getParentPath_should_return_parent_path() {
 		assertThat( RecheckSeleniumAdapter.getParentPath( "//HTML[1]/BODY[1]" ) ).isEqualTo( "//HTML[1]" );
 	}
 
 	@Test
-	public void convertToPeers_should_result_in_valid_tree() throws Exception {
+	void convertToPeers_should_result_in_valid_tree() throws Exception {
 		final Map<String, Map<String, String>> input = new HashMap<>();
 		input.put( "//HTML[1]/BODY[1]", toHashMap( "BODY" ) );
 		input.put( "//HTML[1]/BODY[1]/FOOTER[1]/DIV[1]", toHashMap( "DIV" ) );
