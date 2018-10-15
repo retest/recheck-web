8f99a75e3bd27a9da7a47d53d7a06aed08576f57
Marius Kemmling <marius.kemmling@retest.de>
2018-10-15 14:41:08 +0200

    Update the old test case PageFrameIT to the new standard

--- src/test/java/de/retest/web/PageFrameIT.java
+++ src/test/java/de/retest/web/PageFrameIT.java
@@ -5,42 +5,38 @@ import java.nio.file.Paths;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import de.retest.recheck.Recheck;
 import de.retest.recheck.RecheckImpl;
-import de.retest.web.testutils.ChromeOptionsFactory;
 
 public class PageFrameIT {
 
-	WebDriver driver;
 	Recheck re;
 
 	@BeforeEach
 	void setUp() {
-		driver = new ChromeDriver( ChromeOptionsFactory.createNewInstance() );
-
 		re = new RecheckImpl();
 	}
 
-	@Test
-	void page_frame_html_should_be_checked() throws Exception {
-		re.startTest( "page-frame" );
+	@ParameterizedTest
+	@MethodSource( "de.retest.web.testutils.WebDriverFactory#drivers" )
+	void page_frame_html_should_be_checked( final WebDriver driver ) throws Exception {
+		re.startTest( "page-frame-" + driver.getClass().getSimpleName() );
 
 		final Path simplePagePath = Paths.get( "src/test/resources/pages/page-frame.html" );
 		driver.get( simplePagePath.toUri().toURL().toString() );
 
 		re.check( driver, "open" );
 
-		re.capTest();
+		driver.quit();
 	}
 
 	@AfterEach
 	void tearDown() {
-		driver.quit();
-
+		re.capTest();
 		re.cap();
 	}
 
