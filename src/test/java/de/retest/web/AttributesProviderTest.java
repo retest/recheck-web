dff710670a62ce1bfc6a695178e612cb2c2d4722
Daniel Kraus <daniel.kraus@retest.de>
2018-10-19 16:49:44 +0200

    Improve assertion

--- src/test/java/de/retest/web/AttributesProviderTest.java
+++ src/test/java/de/retest/web/AttributesProviderTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.parallel.ResourceAccessMode.READ_WRITE;
 import static org.junit.jupiter.api.parallel.Resources.SYSTEM_PROPERTIES;
 
+import java.io.UncheckedIOException;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,11 +26,11 @@ class AttributesProviderTest {
 
 	@Test
 	@ResourceLock( value = SYSTEM_PROPERTIES, mode = READ_WRITE )
-	void invalid_attributes_file_should_yield_RuntimeException() throws Exception {
+	void invalid_attributes_file_should_yield_UncheckedIOException() throws Exception {
 		final String attributesFile = "foo";
 		System.setProperty( AttributesProvider.ATTRIBUTES_FILE_PROPERTY, attributesFile );
 		assertThatThrownBy( AttributesProvider::getTestInstance ) //
-				.isInstanceOf( RuntimeException.class ) //
+				.isExactlyInstanceOf( UncheckedIOException.class ) //
 				.hasMessage( "Cannot read attributes file '" + attributesFile + "'." );
 		System.clearProperty( AttributesProvider.ATTRIBUTES_FILE_PROPERTY );
 	}
