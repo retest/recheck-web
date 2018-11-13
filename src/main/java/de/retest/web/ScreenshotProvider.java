bdf6a2b84b64819d862c957be41576aaf418ef7a
Jeremias Roessler <roessler@retest.de>
2018-11-13 13:56:39 +0100

    Adress sonar cloud issues

--- src/main/java/de/retest/web/ScreenshotProvider.java
+++ src/main/java/de/retest/web/ScreenshotProvider.java
@@ -3,6 +3,7 @@ package de.retest.web;
 import java.awt.image.BufferedImage;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.assertthat.selenium_shutterbug.core.Shutterbug;
 import com.assertthat.selenium_shutterbug.utils.web.ScrollStrategy;
@@ -12,8 +13,12 @@ public class ScreenshotProvider {
 	private static final int SCROLL_TIMEOUT = 100;
 	private static final boolean USE_DEVICE_PIXEL_RATIO = true;
 
+	private ScreenshotProvider() {
+		// private constructor for util class
+	}
+
 	public static BufferedImage shootFullPage( final WebDriver driver ) {
-		if ( driver.getClass().getSimpleName().equals( "ChromeDriver" ) ) {
+		if ( driver instanceof ChromeDriver ) {
 			return Shutterbug
 					.shootPage( driver, ScrollStrategy.WHOLE_PAGE_CHROME, SCROLL_TIMEOUT, USE_DEVICE_PIXEL_RATIO )
 					.getImage();
