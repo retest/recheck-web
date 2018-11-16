d085a73180f4c4913d7d45d8ccbed0809e8a13a9
Marius Kemmling <marius.kemmling@retest.de>
2018-11-16 14:52:41 +0100

    Preparation to detect retina displays
    is also easy to extend for other HiDPI displays

--- src/main/java/de/retest/web/ScreenshotProvider.java
+++ src/main/java/de/retest/web/ScreenshotProvider.java
@@ -1,9 +1,13 @@
 package de.retest.web;
 
 import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.lang.reflect.Field;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,6 +19,7 @@ public class ScreenshotProvider {
 	private static final int SCROLL_TIMEOUT = 100;
 	private static final boolean USE_DEVICE_PIXEL_RATIO = true;
 	private static final int WINDOW_WIDTH = 1200;
+	private static final int RETINA_SCALE_FACTOR = 2;
 
 	private ScreenshotProvider() {
 		// private constructor for util class
@@ -42,5 +47,23 @@ public class ScreenshotProvider {
 		graphics2D.drawImage( tmp, 0, 0, null );
 		graphics2D.dispose();
 		return resized;
+	}
+
+	public static boolean isRetinaDisplay() {
+		if ( SystemUtils.IS_OS_MAC ) {
+			final GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			final GraphicsDevice device = environment.getDefaultScreenDevice();
+			try {
+				final Field field = device.getClass().getDeclaredField( "scale" );
+				if ( field != null ) {
+					field.setAccessible( true );
+					final Object scale = field.get( device );
+					if ( ((Integer) scale).intValue() == RETINA_SCALE_FACTOR ) {
+						return true;
+					}
+				}
+			} catch ( final Exception ignore ) {}
+		}
+		return false;
 	}
 }
