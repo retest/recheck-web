a2d8cc27c0369f3d4dbc3258fe5e8592b7d12944
Marius Kemmling <marius.kemmling@retest.de>
2018-11-16 17:10:58 +0100

    Preparation to detect retina displays

--- src/main/java/de/retest/web/ScreenshotProvider.java
+++ src/main/java/de/retest/web/ScreenshotProvider.java
@@ -1,7 +1,13 @@
 package de.retest.web;
 
+import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.lang.reflect.Field;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,6 +18,7 @@ public class ScreenshotProvider {
 
 	private static final int SCROLL_TIMEOUT = 100;
 	private static final boolean USE_DEVICE_PIXEL_RATIO = true;
+	private static final int RETINA_SCALE_FACTOR = 2;
 
 	private ScreenshotProvider() {
 		// private constructor for util class
@@ -27,5 +34,21 @@ public class ScreenshotProvider {
 					.shootPage( driver, ScrollStrategy.BOTH_DIRECTIONS, SCROLL_TIMEOUT, USE_DEVICE_PIXEL_RATIO )
 					.getImage();
 		}
+	public static boolean isRetinaDisplay() {
+		if ( SystemUtils.IS_OS_MAC ) {
+			final GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			final GraphicsDevice device = environment.getDefaultScreenDevice();
+			try {
+				final Field field = device.getClass().getDeclaredField( "scale" );
+				if ( field != null ) {
+					field.setAccessible( true );
+					final Object scale = field.get( device );
+					if ( ((Integer) scale).intValue() == RETINA_SCALE_FACTOR ) {
+						return true;
+					}
+				}
+			} catch ( final Exception ignore ) {}
+		}
+		return false;
 	}
 }
