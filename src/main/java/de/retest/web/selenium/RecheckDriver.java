7ae4f690faeb66c6a1b0718a29c008a2e308a738
Martin Vietz <git@martin.vietz.eu>
2018-11-25 11:42:38 +0100

    Use RemoteWebDriver for wrapped to prevent class cast erros on runtime

--- src/main/java/de/retest/web/selenium/RecheckDriver.java
+++ src/main/java/de/retest/web/selenium/RecheckDriver.java
@@ -1,5 +1,6 @@
 package de.retest.web.selenium;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -13,16 +14,11 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.html5.LocalStorage;
-import org.openqa.selenium.html5.Location;
-import org.openqa.selenium.html5.LocationContext;
-import org.openqa.selenium.html5.SessionStorage;
-import org.openqa.selenium.html5.WebStorage;
 import org.openqa.selenium.interactions.HasInputDevices;
-import org.openqa.selenium.interactions.HasTouchScreen;
+import org.openqa.selenium.interactions.Interactive;
 import org.openqa.selenium.interactions.Keyboard;
 import org.openqa.selenium.interactions.Mouse;
-import org.openqa.selenium.interactions.TouchScreen;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.internal.FindsByClassName;
 import org.openqa.selenium.internal.FindsByCssSelector;
 import org.openqa.selenium.internal.FindsById;
@@ -30,6 +26,7 @@ import org.openqa.selenium.internal.FindsByLinkText;
 import org.openqa.selenium.internal.FindsByName;
 import org.openqa.selenium.internal.FindsByTagName;
 import org.openqa.selenium.internal.FindsByXPath;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,16 +35,16 @@ import de.retest.ui.descriptors.RootElement;
 import de.retest.web.RecheckWebImpl;
 
 public class RecheckDriver implements WebDriver, JavascriptExecutor, FindsById, FindsByClassName, FindsByLinkText,
-		FindsByName, FindsByCssSelector, FindsByTagName, FindsByXPath, HasInputDevices, HasCapabilities,
-		TakesScreenshot, LocationContext, WebStorage, HasTouchScreen {
+		FindsByName, FindsByCssSelector, FindsByTagName, FindsByXPath, HasInputDevices, HasCapabilities, Interactive,
+		TakesScreenshot {
 
 	private static final Logger logger = LoggerFactory.getLogger( RecheckDriver.class );
 
-	private final WebDriver wrapped;
+	private final RemoteWebDriver wrapped;
 	private RootElement lastExpectedState;
 	private RootElement lastActualState;
 
-	public RecheckDriver( final WebDriver wrapped ) {
+	public RecheckDriver( final RemoteWebDriver wrapped ) {
 		this.wrapped = wrapped;
 	}
 
@@ -103,82 +100,82 @@ public class RecheckDriver implements WebDriver, JavascriptExecutor, FindsById, 
 
 	@Override
 	public WebElement findElementById( final String using ) {
-		return ((FindsById) wrapped).findElementById( using );
+		return wrapped.findElementById( using );
 	}
 
 	@Override
 	public List<WebElement> findElementsById( final String using ) {
-		return ((FindsById) wrapped).findElementsById( using );
+		return wrapped.findElementsById( using );
 	}
 
 	@Override
 	public WebElement findElementByClassName( final String using ) {
-		return ((FindsByClassName) wrapped).findElementByClassName( using );
+		return wrapped.findElementByClassName( using );
 	}
 
 	@Override
 	public List<WebElement> findElementsByClassName( final String using ) {
-		return ((FindsByClassName) wrapped).findElementsByClassName( using );
+		return wrapped.findElementsByClassName( using );
 	}
 
 	@Override
 	public WebElement findElementByLinkText( final String using ) {
-		return ((FindsByLinkText) wrapped).findElementByLinkText( using );
+		return wrapped.findElementByLinkText( using );
 	}
 
 	@Override
 	public List<WebElement> findElementsByLinkText( final String using ) {
-		return ((FindsByLinkText) wrapped).findElementsByLinkText( using );
+		return wrapped.findElementsByLinkText( using );
 	}
 
 	@Override
 	public WebElement findElementByPartialLinkText( final String using ) {
-		return ((FindsByLinkText) wrapped).findElementByPartialLinkText( using );
+		return wrapped.findElementByPartialLinkText( using );
 	}
 
 	@Override
 	public List<WebElement> findElementsByPartialLinkText( final String using ) {
-		return ((FindsByLinkText) wrapped).findElementsByPartialLinkText( using );
+		return wrapped.findElementsByPartialLinkText( using );
 	}
 
 	@Override
 	public WebElement findElementByName( final String using ) {
-		return ((FindsByName) wrapped).findElementByName( using );
+		return wrapped.findElementByName( using );
 	}
 
 	@Override
 	public List<WebElement> findElementsByName( final String using ) {
-		return ((FindsByName) wrapped).findElementsByName( using );
+		return wrapped.findElementsByName( using );
 	}
 
 	@Override
 	public WebElement findElementByCssSelector( final String using ) {
-		return ((FindsByCssSelector) wrapped).findElementByCssSelector( using );
+		return wrapped.findElementByCssSelector( using );
 	}
 
 	@Override
 	public List<WebElement> findElementsByCssSelector( final String using ) {
-		return ((FindsByCssSelector) wrapped).findElementsByCssSelector( using );
+		return wrapped.findElementsByCssSelector( using );
 	}
 
 	@Override
 	public WebElement findElementByTagName( final String using ) {
-		return ((FindsByTagName) wrapped).findElementByTagName( using );
+		return wrapped.findElementByTagName( using );
 	}
 
 	@Override
 	public List<WebElement> findElementsByTagName( final String using ) {
-		return ((FindsByTagName) wrapped).findElementsByTagName( using );
+		return wrapped.findElementsByTagName( using );
 	}
 
 	@Override
 	public WebElement findElementByXPath( final String using ) {
-		return ((FindsByXPath) wrapped).findElementByXPath( using );
+		return wrapped.findElementByXPath( using );
 	}
 
 	@Override
 	public List<WebElement> findElementsByXPath( final String using ) {
-		return ((FindsByXPath) wrapped).findElementsByXPath( using );
+		return wrapped.findElementsByXPath( using );
 	}
 
 	@Override
@@ -238,56 +235,41 @@ public class RecheckDriver implements WebDriver, JavascriptExecutor, FindsById, 
 
 	@Override
 	public Object executeScript( final String script, final Object... args ) {
-		return ((JavascriptExecutor) wrapped).executeScript( script, args );
+		return wrapped.executeScript( script, args );
 	}
 
 	@Override
 	public Object executeAsyncScript( final String script, final Object... args ) {
-		return ((JavascriptExecutor) wrapped).executeAsyncScript( script, args );
+		return wrapped.executeAsyncScript( script, args );
 	}
 
 	@Override
 	public <X> X getScreenshotAs( final OutputType<X> target ) throws WebDriverException {
-		return ((TakesScreenshot) wrapped).getScreenshotAs( target );
+		return wrapped.getScreenshotAs( target );
 	}
 
 	@Override
 	public Keyboard getKeyboard() {
-		return ((HasInputDevices) wrapped).getKeyboard();
+		return wrapped.getKeyboard();
 	}
 
 	@Override
 	public Mouse getMouse() {
-		return ((HasInputDevices) wrapped).getMouse();
+		return wrapped.getMouse();
 	}
 
 	@Override
 	public Capabilities getCapabilities() {
-		return ((HasCapabilities) wrapped).getCapabilities();
+		return wrapped.getCapabilities();
 	}
 
 	@Override
-	public Location location() {
-		return ((LocationContext) wrapped).location();
+	public void perform( final Collection<Sequence> actions ) {
+		wrapped.perform( actions );
 	}
 
 	@Override
-	public void setLocation( final Location location ) {
-		((LocationContext) wrapped).setLocation( location );
-	}
-
-	@Override
-	public LocalStorage getLocalStorage() {
-		return ((WebStorage) wrapped).getLocalStorage();
-	}
-
-	@Override
-	public SessionStorage getSessionStorage() {
-		return ((WebStorage) wrapped).getSessionStorage();
-	}
-
-	@Override
-	public TouchScreen getTouch() {
-		return ((HasTouchScreen) wrapped).getTouch();
+	public void resetInputState() {
+		wrapped.resetInputState();
 	}
 }
