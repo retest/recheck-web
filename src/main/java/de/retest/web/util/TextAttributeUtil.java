a3e686a7a116836dd9875f86018fd13e11281adb
Enes Özel <enes.oezel@retest.de>
2019-02-08 15:01:06 +0100

    Improve method
    so it return pre elements unformatted

--- src/main/java/de/retest/web/util/TextAttributeUtil.java
+++ src/main/java/de/retest/web/util/TextAttributeUtil.java
@@ -1,16 +1,18 @@
 package de.retest.web.util;
 
+import static de.retest.web.AttributesConfig.TEXT;
+
 import de.retest.ui.descriptors.TextAttribute;
-import de.retest.web.AttributesConfig;
 
 public class TextAttributeUtil {
 
-	public static TextAttribute createTextAttribute( final String text ) {
-		return new TextAttribute( AttributesConfig.TEXT, cleanString( text ) );
+	public static TextAttribute createTextAttribute( final String path, final String text ) {
+		return path.toLowerCase().contains( "pre" ) ? new TextAttribute( TEXT, text )
+				: new TextAttribute( TEXT, cleanString( text ) );
 	}
 
 	private static String cleanString( final String text ) {
-		return text.replaceAll( "\\s+", " " );
+		return text.replaceAll( "\\s+", " " ).trim();
 	}
 
 }
