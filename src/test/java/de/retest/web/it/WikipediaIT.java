9a3642745595c279cd756950c6266ef4801737fa
Daniel Kraus <daniel.kraus@retest.de>
2019-03-08 14:18:31 +0100

    Add (local) Wikipedia test along with golden master

---
+++ src/test/java/de/retest/web/it/WikipediaIT.java
@@ -0,0 +1,41 @@
+package de.retest.web.it;
+
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import de.retest.recheck.Recheck;
+import de.retest.recheck.RecheckImpl;
+
+class WikipediaIT {
+
+	WebDriver driver;
+	Recheck re;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		driver = new ChromeDriver();
+		re = new RecheckImpl();
+	}
+
+	@Test
+	void myWikipediaTest() throws Exception {
+		re.startTest();
+		final String url = Paths.get( "src/test/resources/pages/wikipedia/wikipedia-characterization-test.html" )
+				.toUri().toURL().toString();
+		driver.get( url );
+		re.check( driver, "characterization-testing-page" );
+		re.capTest();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		driver.close();
+		re.cap();
+	}
+
+}
