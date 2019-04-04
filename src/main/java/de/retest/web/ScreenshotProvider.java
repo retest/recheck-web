05695433cb3c22e0eb5aecac77832aae04ae0401
Daniel Kraus <daniel.kraus@retest.de>
2019-04-04 17:18:15 +0200

    Split shoot into full page and viewport only

--- src/main/java/de/retest/web/ScreenshotProvider.java
+++ src/main/java/de/retest/web/ScreenshotProvider.java
@@ -13,6 +13,7 @@ import com.assertthat.selenium_shutterbug.utils.web.ScrollStrategy;
 
 public class ScreenshotProvider {
 
+	private static final String VIEWPORT_ONLY_SCREENSHOT_PROPERTY = "de.retest.recheck.web.viewportOnlyScreenshot";
 	private static final int SCROLL_TIMEOUT_MS = 100;
 	private static final boolean USE_DEVICE_PIXEL_RATIO = true;
 
@@ -23,6 +24,11 @@ public class ScreenshotProvider {
 	}
 
 	public static BufferedImage shoot( final WebDriver driver ) {
+		final boolean viewportOnly = Boolean.getBoolean( VIEWPORT_ONLY_SCREENSHOT_PROPERTY );
+		return viewportOnly ? shootViewportOnly( driver ) : shootFullPage( driver );
+	}
+
+	private static BufferedImage shootFullPage( final WebDriver driver ) {
 		if ( driver instanceof ChromeDriver ) {
 			final BufferedImage image = Shutterbug //
 					.shootPage( driver, ScrollStrategy.WHOLE_PAGE_CHROME, SCROLL_TIMEOUT_MS, USE_DEVICE_PIXEL_RATIO ) //
@@ -33,4 +39,9 @@ public class ScreenshotProvider {
 				.shootPage( driver, ScrollStrategy.BOTH_DIRECTIONS, SCROLL_TIMEOUT_MS, USE_DEVICE_PIXEL_RATIO ) //
 				.getImage();
 	}
+
+	private static BufferedImage shootViewportOnly( final WebDriver driver ) {
+		return Shutterbug.shootPage( driver, USE_DEVICE_PIXEL_RATIO ).getImage();
+	}
+
 }
