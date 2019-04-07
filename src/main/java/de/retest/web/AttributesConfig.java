62b09ee8b9267b5889f05781001a7639b23dde20
Daniel Kraus <daniel.kraus@retest.de>
2019-04-07 18:44:41 +0200

    Add missing identifying attributes from YAML

--- src/main/java/de/retest/web/AttributesConfig.java
+++ src/main/java/de/retest/web/AttributesConfig.java
@@ -18,6 +18,11 @@ public class AttributesConfig {
 	public static final String HEIGHT = "height";
 
 	public static final String TAG_NAME = "tagName";
+
+	// from attributes.yaml
+	public static final String CLASS = "class";
+	public static final String ID = "id";
+	public static final String NAME = "name";
 	public static final String TEXT = "text";
 
 	private final List<String> cssAttributes;
