78ed941d00dc9bce8c08f1e82189093ce1d9fb1e
Jeremias Roessler <roessler@retest.de>
2019-04-13 00:19:29 +0200

    Improve test after review

--- src/test/java/de/retest/web/selenium/AutocheckingRecheckDriverTest.java
+++ src/test/java/de/retest/web/selenium/AutocheckingRecheckDriverTest.java
@@ -1,19 +1,18 @@
 package de.retest.web.selenium;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 class AutocheckingRecheckDriverTest {
 
 	@Test
-	void test_makeUnique() {
-		final AutocheckingRecheckDriver cut = new AutocheckingRecheckDriver( Mockito.mock( RemoteWebDriver.class ) );
+	void makeUnique_should_append_unique_prefix() {
+		final AutocheckingRecheckDriver cut = new AutocheckingRecheckDriver( mock( RemoteWebDriver.class ) );
 		assertThat( cut.makeUnique( "someString" ) ).isEqualTo( "00_someString" );
 		assertThat( cut.makeUnique( "someOtherString" ) ).isEqualTo( "01_someOtherString" );
 		assertThat( cut.makeUnique( "someString" ) ).isEqualTo( "02_someString" );
 	}
-
 }
