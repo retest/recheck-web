c395efb546f407e867594ad60199c5cb5994e48c
Jeremias Roessler <roessler@retest.de>
2019-04-18 20:14:21 +0200

    Readd a RecheckDriver if someone was already using it
    This should always point to the "default" implementation.

---
+++ src/main/java/de/retest/web/selenium/RecheckDriver.java
@@ -0,0 +1,29 @@
+package de.retest.web.selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import de.retest.recheck.Recheck;
+import de.retest.recheck.RecheckOptions;
+
+/**
+ * This is a wrapper for different {@link RemoteWebDriver} implementations, such as {@link ChromeDriver} or
+ * {@link FirefoxDriver}.
+ *
+ * The RecheckDriver extends AutocheckingDriver, such that it is unnecessary to call
+ * {@link Recheck#check(Object, String)} explicitly. It also extends {@link UnbreakableDriver}, such that it will not
+ * break on otherwise breaking changes until these changes are also applied to the Golden Master.
+ */
+public class RecheckDriver extends AutocheckingRecheckDriver {
+
+	public RecheckDriver( final RemoteWebDriver wrapped ) {
+		super( wrapped );
+	}
+
+	public RecheckDriver( final RemoteWebDriver wrapped, final RecheckOptions options,
+			final AutocheckingCheckNamingStrategy namingStrategy ) {
+		super( wrapped, options, namingStrategy );
+	}
+
+}
