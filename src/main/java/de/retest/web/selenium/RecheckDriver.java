d255939a6f592680a02f476e512e4e7431a02adb
Jeremias Roessler <roessler@retest.de>
2019-04-23 10:25:42 +0200

    Improvements after review

--- src/main/java/de/retest/web/selenium/RecheckDriver.java
+++ src/main/java/de/retest/web/selenium/RecheckDriver.java
@@ -11,7 +11,7 @@ import de.retest.recheck.RecheckOptions;
  * This is a wrapper for different {@link RemoteWebDriver} implementations, such as {@link ChromeDriver} or
  * {@link FirefoxDriver}.
  *
- * The RecheckDriver extends AutocheckingDriver, such that it is unnecessary to call
+ * The {@code RecheckDriver} extends {@code AutocheckingDriver}, such that it is unnecessary to call
  * {@link Recheck#check(Object, String)} explicitly. It also extends {@link UnbreakableDriver}, such that it will not
  * break on otherwise breaking changes until these changes are also applied to the Golden Master.
  */
