75c058e3d005c252f465d6c3d28eec982997f3a5
Daniel Kraus <daniel.kraus@retest.de>
2019-05-06 11:05:49 +0200

    Improve Javadoc

--- src/main/java/de/retest/web/mapping/WebData.java
+++ src/main/java/de/retest/web/mapping/WebData.java
@@ -12,6 +12,17 @@ import org.slf4j.LoggerFactory;
 import de.retest.web.AttributesUtil;
 import de.retest.web.ConversionException;
 
+/**
+ * Mapping of key-value pairs for attributes:
+ *
+ * <pre>
+ * {
+ *   attribute0 = value0,
+ *   attribute1 = value1,
+ *   ...
+ * }
+ * </pre>
+ */
 public class WebData {
 
 	private static final Logger logger = LoggerFactory.getLogger( WebData.class );
@@ -24,8 +35,8 @@ public class WebData {
 
 	/**
 	 * @param key
-	 *            the data key
-	 * @return the string value
+	 *            the attribute key
+	 * @return the attribute value as string
 	 */
 	public String getAsString( final String key ) {
 		final Object result = wrappedData.get( key );
@@ -91,10 +102,10 @@ public class WebData {
 
 	/**
 	 * @param key
-	 *            the data key
-	 * @return the int value
+	 *            the attribute key
+	 * @return the attribute value as integer
 	 * @throws ConversionException
-	 *             if the value for the given key cannot be converted to int
+	 *             if the value for the given key cannot be converted to an integer
 	 */
 	public int getAsInt( final String key ) {
 		final Object value = wrappedData.get( key );
