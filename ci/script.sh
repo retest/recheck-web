fa88a0024d7d7d0d34d83aac95d24371c5d116bb
Daniel Kraus <daniel.kraus@retest.de>
2019-05-21 21:30:04 +0200

    Compile with 8 and test with 11

--- ci/script.sh
+++ ci/script.sh
@@ -10,7 +10,13 @@ export $(dbus-launch)
 export NSS_USE_SHARED_DB=ENABLED
 export MVN="mvn --settings ${TRAVIS_BUILD_DIR}/.travis.settings.xml"
 
-###### Maven
+###### Maven ######
+# Compile with JDK 8
+${MVN} clean package -DskipTests
+
+# Test with JDK 11
+wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 11
+
 if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
     ${MVN} clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar
 else
