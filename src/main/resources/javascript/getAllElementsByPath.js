4e17942b36c3172d3fec6e4157ed5d75c3ae1095
Jeremias Roessler <roessler@retest.de>
2019-06-17 20:55:31 +0200

    Also check whether input is disabled

--- src/main/resources/javascript/getAllElementsByPath.js
+++ src/main/resources/javascript/getAllElementsByPath.js
@@ -52,6 +52,19 @@ function addCoordinates(extractedAttributes, node) {
 	}
 }
 
+function getDisabled(node) {
+	if (!node.disabled) {
+		return false;
+	}
+	if (node.disabled === "") {
+		return false;
+	}
+	if (node.disabled === "disabled") {
+		return true;
+	}
+	return node.disabled;
+}
+
 function transform(node) {
 	var extractedAttributes = {
 		"tagName": node.tagName.toLowerCase(),
@@ -73,6 +86,9 @@ function transform(node) {
 		var attributeValue = attrs[i].value;
 		extractedAttributes[attributeName] = attributeValue;
 	}
+	
+	// overwrite empty attributes (e.g. 'disabled')
+	extractedAttributes["disabled"] = getDisabled(node);
 
 	// extract *given* CSS style attributes
 	var style = window.getComputedStyle(node);
