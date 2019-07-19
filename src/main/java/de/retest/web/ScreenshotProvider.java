15caffb9081fb349bacbe2c42f29c46d6a87c6d0
Daniel Kraus <daniel.kraus@retest.de>
2019-07-19 17:15:56 +0200

    Unwrap driver for screenshots

--- src/main/java/de/retest/web/ScreenshotProvider.java
+++ src/main/java/de/retest/web/ScreenshotProvider.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import com.assertthat.selenium_shutterbug.core.Shutterbug;
 import com.assertthat.selenium_shutterbug.utils.web.ScrollStrategy;
 
+import de.retest.web.selenium.UnbreakableDriver;
+
 public class ScreenshotProvider {
 
 	private static final String VIEWPORT_ONLY_SCREENSHOT_PROPERTY = "de.retest.recheck.web.viewportOnlyScreenshot";
@@ -23,7 +25,8 @@ public class ScreenshotProvider {
 
 	public static BufferedImage shoot( final WebDriver driver ) {
 		final boolean viewportOnly = Boolean.getBoolean( VIEWPORT_ONLY_SCREENSHOT_PROPERTY );
-		return viewportOnly ? shootViewportOnly( driver ) : shootFullPage( driver );
+		final WebDriver wrapped = unwrap( driver );
+		return viewportOnly ? shootViewportOnly( wrapped ) : shootFullPage( wrapped );
 	}
 
 	private static BufferedImage shootFullPage( final WebDriver driver ) {
@@ -35,6 +38,18 @@ public class ScreenshotProvider {
 
 	private static BufferedImage shootViewportOnly( final WebDriver driver ) {
 		return Shutterbug.shootPage( driver, USE_DEVICE_PIXEL_RATIO ).getImage();
+	}
+
+	/**
+	 * Unwrap the actual {@code WebDriver} if present. Since Shutterbug uses {@code instanceof} checks, these would fail
+	 * in the case of {@code UnbreakableDriver}.
+	 *
+	 * @param driver
+	 *            The driver to unwrap.
+	 * @return The wrapped driver if present, otherwise the given driver.
+	 */
+	private static WebDriver unwrap( final WebDriver driver ) {
+		return driver instanceof UnbreakableDriver ? ((UnbreakableDriver) driver).getWrapped() : driver;
 	}
 
 }
