3f658a73328a272ff89a886cf510f8c67fba06c6
IvanKaradzhov <ivan.karadzhov@retest.de>
2019-07-22 11:16:28 +0200

    Write tests for the breaking message methods and improve the other tests

--- src/test/java/de/retest/web/selenium/TestHealerTest.java
+++ src/test/java/de/retest/web/selenium/TestHealerTest.java
@@ -8,29 +8,58 @@ import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import de.retest.recheck.report.ActionReplayResult;
 import de.retest.recheck.ui.descriptors.Attributes;
 import de.retest.recheck.ui.descriptors.Element;
 import de.retest.recheck.ui.descriptors.IdentifyingAttributes;
 import de.retest.recheck.ui.descriptors.MutableAttributes;
 import de.retest.recheck.ui.descriptors.RootElement;
+import de.retest.recheck.ui.diff.ElementDifference;
+import de.retest.recheck.ui.diff.StateDifference;
 
 class TestHealerTest {
 
-	@Test
-	public void ByCssSelector_matches_elements_with_given_class() {
-		final RecheckDriver wrapped = mock( RecheckDriver.class );
-		final WebElement resultMarker = mock( WebElement.class );
+	RecheckDriver wrapped;
+	WebElement resultMarker;
+	RootElement state;
+	ActionReplayResult actionReplayResult;
+	ElementDifference elementDifference;
+	ElementDifference otherElementDifference;
+	List<ElementDifference> elementDifferences;
+	StateDifference stateDifference;
 
-		final RootElement state = mock( RootElement.class );
+	@BeforeEach
+	void setUp() {
+		wrapped = mock( RecheckDriver.class );
+		resultMarker = mock( WebElement.class );
+		state = mock( RootElement.class );
+		actionReplayResult = mock( ActionReplayResult.class );
+		elementDifference = mock( ElementDifference.class );
+		otherElementDifference = mock( ElementDifference.class );
+		stateDifference = mock( StateDifference.class );
+		elementDifferences = new ArrayList<>();
+		elementDifferences.add( elementDifference );
+		elementDifferences.add( otherElementDifference );
 		when( wrapped.getLastExpectedState() ).thenReturn( state );
 		when( wrapped.getLastActualState() ).thenReturn( state );
+		when( wrapped.getLastActionReplayResult() ).thenReturn( actionReplayResult );
+		when( wrapped.getLastActionReplayResult().getStateDifference() ).thenReturn( stateDifference );
+		when( stateDifference.getElementDifferences() ).thenReturn( elementDifferences );
+		when( elementDifference.getRetestId() ).thenReturn( "retestId" );
+		when( otherElementDifference.getRetestId() ).thenReturn( "retestId" );
+	}
 
+	@Test
+	public void ByCssSelector_matches_elements_with_given_class() {
 		final MutableAttributes attributes = new MutableAttributes();
 		attributes.put( "class", "pure-button my-button menu-button" );
 
@@ -48,11 +77,6 @@ class TestHealerTest {
 
 	@Test
 	public void not_yet_implemented_ByCssSelector_should_throw_exception() {
-		final RecheckDriver wrapped = mock( RecheckDriver.class );
-		final RootElement state = mock( RootElement.class );
-		when( wrapped.getLastExpectedState() ).thenReturn( state );
-		when( wrapped.getLastActualState() ).thenReturn( state );
-
 		try {
 			findElement( By.cssSelector( ".open > .dropdown-toggle.btn-primary" ), wrapped );
 			fail( "Excpected exception" );
@@ -87,13 +111,6 @@ class TestHealerTest {
 
 	@Test
 	public void ByXPathExpression_matches_elements_with_given_xpath() {
-		final RecheckDriver wrapped = mock( RecheckDriver.class );
-		final WebElement resultMarker = mock( WebElement.class );
-
-		final RootElement state = mock( RootElement.class );
-		when( wrapped.getLastExpectedState() ).thenReturn( state );
-		when( wrapped.getLastActualState() ).thenReturn( state );
-
 		final String xpath = "HTML[1]/DIV[3]/DIV[3]/DIV[3]/DIV[2]";
 		final IdentifyingAttributes identifying = IdentifyingAttributes.create( fromString( xpath ), "DIV" );
 		final Element element = create( "id", state, identifying, new Attributes() );
@@ -108,11 +125,6 @@ class TestHealerTest {
 
 	@Test
 	public void not_yet_implemented_ByXPathExpression_should_throw_exception() {
-		final RecheckDriver wrapped = mock( RecheckDriver.class );
-		final RootElement state = mock( RootElement.class );
-		when( wrapped.getLastExpectedState() ).thenReturn( state );
-		when( wrapped.getLastActualState() ).thenReturn( state );
-
 		try {
 			findElement( By.xpath( "//div[@id='mw-content-text']/div[2]" ), wrapped );
 			fail( "Excpected exception" );
