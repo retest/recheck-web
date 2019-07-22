6cae3997041a25fc8fc7b10bd39bd9eb181ce9f8
Johannes Dillmann <johannes.dillmann@retest.de>
2019-07-22 14:21:39 +0200

    Add 30s timeout for upload

--- ci/upload.sh
+++ ci/upload.sh
@@ -15,7 +15,7 @@ echo ${RCLONE_HASH} "rclone-${RCLONE_VERSION}-linux-amd64.zip" | md5sum --check 
 unzip -j -q "rclone-${RCLONE_VERSION}-linux-amd64.zip" "rclone-${RCLONE_VERSION}-linux-amd64/rclone" -d ${HOME}/bin/
 
 echo "Uploading to S3 ..."
-rclone copy ${TRAVIS_BUILD_DIR}/${SOURCE_PATH} ${BUCKET}/${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}/source
-rclone copy ${TRAVIS_BUILD_DIR}/${TARGET_PATH} ${BUCKET}/${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}/target
+rclone --timeout 30s copy ${TRAVIS_BUILD_DIR}/${SOURCE_PATH} ${BUCKET}/${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}/source
+rclone --timeout 30s copy ${TRAVIS_BUILD_DIR}/${TARGET_PATH} ${BUCKET}/${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}/target
 
 exit 0
