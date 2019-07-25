10355e697f45b339d074e8b0f9dfd37d553defef
Daniel Kraus <daniel.kraus@retest.de>
2019-07-25 23:41:06 +0200

    Extract method

--- src/main/java/de/retest/web/FrameConverter.java
+++ src/main/java/de/retest/web/FrameConverter.java
@@ -28,37 +28,41 @@ public class FrameConverter {
 
 	public void addChildrenFromFrames( final WebDriver driver, final Set<String> cssAttributes,
 			final RootElement lastChecked ) {
-		final JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
 		final List<Element> frames =
 				de.retest.web.selenium.By.findElements( lastChecked.getContainedElements(), isFrame() );
 		log.debug( "Found {} frame(s), getting data per frame.", frames.size() );
 		for ( final Element frame : frames ) {
-			final String frameId = frame.getIdentifyingAttributes().get( "id" );
-			if ( frameId == null ) {
-				// TODO Implement handling e.g. via name, XPath, etc.
-				log.error( "Cannot retrieve frame without ID from {}.", frame );
-				continue;
-			}
-			try {
-				log.debug( "Switching to frame with ID '{}'.", frameId );
-				driver.switchTo().frame( frameId );
-				final String framePath = frame.getIdentifyingAttributes().getPath();
-				@SuppressWarnings( "unchecked" )
-				final PathsToWebDataMapping mapping = new PathsToWebDataMapping( framePath,
-						(Map<String, Map<String, Object>>) jsExecutor.executeScript( queryJs, cssAttributes ) );
-				final RootElement frameContent = new PeerConverter( retestIdProvider, attributesProvider, mapping,
-						"frame-" + frameId, null, defaultValueFinder ) {
-					@Override
-					protected boolean isRoot( final String parentPath ) {
-						// handle trailing slashes...
-						return framePath.equals( parentPath.replaceAll( "/$", "" ) );
-					}
-				}.convertToPeers();
-				frame.addChildren( frameContent.getContainedElements() );
-			} catch ( final Exception e ) {
-				log.error( "Exception retrieving data content of frame with ID '{}'.", frameId, e );
-			}
+			addChildrenFromFrame( driver, cssAttributes, frame );
 			driver.switchTo().defaultContent();
+		}
+	}
+
+	private void addChildrenFromFrame( final WebDriver driver, final Set<String> cssAttributes, final Element frame ) {
+		final JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+		final String frameId = frame.getIdentifyingAttributes().get( "id" );
+		if ( frameId == null ) {
+			// TODO Implement handling e.g. via name, XPath, etc.
+			log.error( "Cannot retrieve frame without ID from {}.", frame );
+			return;
+		}
+		try {
+			log.debug( "Switching to frame with ID '{}'.", frameId );
+			driver.switchTo().frame( frameId );
+			final String framePath = frame.getIdentifyingAttributes().getPath();
+			@SuppressWarnings( "unchecked" )
+			final PathsToWebDataMapping mapping = new PathsToWebDataMapping( framePath,
+					(Map<String, Map<String, Object>>) jsExecutor.executeScript( queryJs, cssAttributes ) );
+			final RootElement frameContent = new PeerConverter( retestIdProvider, attributesProvider, mapping,
+					"frame-" + frameId, null, defaultValueFinder ) {
+				@Override
+				protected boolean isRoot( final String parentPath ) {
+					// handle trailing slashes...
+					return framePath.equals( parentPath.replaceAll( "/$", "" ) );
+				}
+			}.convertToPeers();
+			frame.addChildren( frameContent.getContainedElements() );
+		} catch ( final Exception e ) {
+			log.error( "Exception retrieving data content of frame with ID '{}'.", frameId, e );
 		}
 	}
 
