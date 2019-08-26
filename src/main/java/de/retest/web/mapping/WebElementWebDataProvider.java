7c0cd02eb264d3956729ad1bba118b605d3cf422
diba1013 <bastian.diehl@retest.de>
2019-08-26 10:36:01 +0200

    Gather all html attributes

--- src/main/java/de/retest/web/mapping/WebElementWebDataProvider.java
+++ src/main/java/de/retest/web/mapping/WebElementWebDataProvider.java
@@ -1,16 +1,26 @@
 package de.retest.web.mapping;
 
 import java.awt.Rectangle;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WrapsDriver;
 
 import de.retest.web.AttributesProvider;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class WebElementWebDataProvider implements WebDataProvider {
+
+	private static final String GET_ALL_HTML_ATTRIBUTES = "var element = arguments[0];" + "var items = {};" //
+			+ "for (i = 0; i < element.attributes.length; ++i) {" //
+			+ "items[element.attributes[i].name] = element.attributes[i].value" //
+			+ "};" //
+			+ "return items;";
 
 	private final WebElement element;
 	private final String path;
@@ -46,10 +56,27 @@ public class WebElementWebDataProvider implements WebDataProvider {
 	@Override
 	public Stream<Pair<String, String>> getHTMLAttributes() {
 		if ( attributesProvider.allHtmlAttributes() ) {
-			return Stream.empty(); // TODO: Do your best to gather all available html attributes.
+			return getAllHTMLAttributes();
 		}
 		return attributesProvider.getHtmlAttributes().stream() //
 				.map( key -> Pair.of( key, element.getAttribute( key ) ) );
+	}
+
+	private Stream<Pair<String, String>> getAllHTMLAttributes() {
+		if ( element instanceof WrapsDriver ) {
+			final WebDriver driver = ((WrapsDriver) element).getWrappedDriver();
+			if ( driver instanceof JavascriptExecutor ) {
+				final Map<String, String> attributes = executeScript( (JavascriptExecutor) driver );
+				return attributes.entrySet().stream() //
+						.map( entry -> Pair.of( entry.getKey(), entry.getValue() ) );
+			}
+		}
+		return Stream.empty(); // TODO: Do your best to gather all available html attributes.
+	}
+
+	@SuppressWarnings( "unchecked" )
+	private Map<String, String> executeScript( final JavascriptExecutor executor ) {
+		return (Map<String, String>) executor.executeScript( GET_ALL_HTML_ATTRIBUTES, element );
 	}
 
 	@Override
