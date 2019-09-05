f1c45c902abfbbbeac6619c9e26201fd7574c1d3
Jeremias Roessler <roessler@retest.de>
2019-09-05 16:07:53 +0200

    Add some JavaDoc for public API

--- src/main/java/de/retest/web/selenium/RecheckWebOptions.java
+++ src/main/java/de/retest/web/selenium/RecheckWebOptions.java
@@ -5,10 +5,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+/**
+ * This class extends RecheckOptions for some specific web options.
+ */
 @SuperBuilder
 @Getter
 public class RecheckWebOptions extends RecheckOptions {
 
+	/**
+	 * The auto-checking driver needs more info than the standard naming strategy can provide. Therefore we need a
+	 * special {@link AutocheckingCheckNamingStrategy}.
+	 */
 	@Builder.Default
 	private final AutocheckingCheckNamingStrategy namingStrategy = new CounterCheckNamingStrategy();
 
