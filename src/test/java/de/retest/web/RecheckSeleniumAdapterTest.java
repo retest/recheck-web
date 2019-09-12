dc7da2ca0d425cf7baf3ae16b63203c56613770b
Daniel Kraus <daniel.kraus@retest.de>
2019-09-12 09:24:12 +0200

    Update and refactor test

--- src/test/java/de/retest/web/RecheckSeleniumAdapterTest.java
+++ src/test/java/de/retest/web/RecheckSeleniumAdapterTest.java
@@ -3,9 +3,9 @@ package de.retest.web;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.openqa.selenium.WrapsElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -14,13 +14,14 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.RemoteWebElement;
 
+import de.retest.recheck.RecheckAdapter;
 import de.retest.web.selenium.WebElementWrapper;
 
 class RecheckSeleniumAdapterTest {
 
 	@Test
 	void seleniumAdapter_canCheck_Driver() {
-		final RecheckSeleniumAdapter cut = new RecheckSeleniumAdapter();
+		final RecheckAdapter cut = new RecheckSeleniumAdapter();
 
 		assertThat( cut.canCheck( mock( ChromeDriver.class ) ) ).isTrue();
 		assertThat( cut.canCheck( mock( FirefoxDriver.class ) ) ).isTrue();
@@ -31,30 +32,30 @@ class RecheckSeleniumAdapterTest {
 
 	@Test
 	void seleniumAdapter_canCheck_WebElements() {
-		final RecheckSeleniumAdapter cut = new RecheckSeleniumAdapter();
+		final RecheckAdapter cut = new RecheckSeleniumAdapter();
 		assertThat( cut.canCheck( mock( RemoteWebElement.class ) ) ).isTrue();
 
 		final WebElementWrapper mock = mock( WebElementWrapper.class );
-		Mockito.when( mock.getWrappedElement() ).thenReturn( mock( RemoteWebElement.class ) );
+		when( mock.getWrappedElement() ).thenReturn( mock( RemoteWebElement.class ) );
 		assertThat( cut.canCheck( mock ) ).isTrue();
 
 		final WrapsElement mockedWrap = mock( WrapsElement.class );
-		Mockito.when( mockedWrap.getWrappedElement() ).thenReturn( mock( RemoteWebElement.class ) );
+		when( mockedWrap.getWrappedElement() ).thenReturn( mock( RemoteWebElement.class ) );
 		assertThat( cut.canCheck( mockedWrap ) ).isTrue();
 	}
 
 	@Test
 	void seleniumAdapter_can_NOT_Check_Object() {
-		final RecheckSeleniumAdapter cut = new RecheckSeleniumAdapter();
+		final RecheckAdapter cut = new RecheckSeleniumAdapter();
 		assertThat( cut.canCheck( new Object() ) ).isFalse();
 	}
 
 	@Test
 	void convert_invalid_should_throw_exception() {
-		final RecheckSeleniumAdapter cut = new RecheckSeleniumAdapter();
+		final RecheckAdapter cut = new RecheckSeleniumAdapter();
 		assertThatThrownBy( () -> {
 			cut.convert( new Object() );
 		} ).isInstanceOf( IllegalArgumentException.class )
-				.hasMessageContaining( "Cannot convert objects of class java.lang.Object" );
+				.hasMessageContaining( "Cannot convert objects of type 'java.lang.Object'." );
 	}
 }
