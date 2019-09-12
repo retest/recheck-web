63fd43370b6c75c4e91a02859dd5b7254f520244
Daniel Kraus <daniel.kraus@retest.de>
2019-09-12 09:59:18 +0200

    Add more tests

--- src/test/java/de/retest/web/RecheckSeleniumAdapterTest.java
+++ src/test/java/de/retest/web/RecheckSeleniumAdapterTest.java
@@ -9,6 +9,8 @@ import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WrapsDriver;
@@ -17,11 +19,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.safari.SafariDriver;
 
 import de.retest.recheck.RecheckAdapter;
-import de.retest.web.selenium.WebElementWrapper;
+import de.retest.web.selenium.AutocheckingRecheckDriver;
+import de.retest.web.selenium.RecheckDriver;
+import de.retest.web.selenium.UnbreakableDriver;
 
 class RecheckSeleniumAdapterTest {
 
@@ -32,38 +38,29 @@ class RecheckSeleniumAdapterTest {
 		cut = new RecheckSeleniumAdapter();
 	}
 
-	@Test
-	void seleniumAdapter_canCheck_Driver() {
-		assertThat( cut.canCheck( mock( ChromeDriver.class ) ) ).isTrue();
-		assertThat( cut.canCheck( mock( FirefoxDriver.class ) ) ).isTrue();
-		assertThat( cut.canCheck( mock( EdgeDriver.class ) ) ).isTrue();
-		assertThat( cut.canCheck( mock( InternetExplorerDriver.class ) ) ).isTrue();
-		assertThat( cut.canCheck( mock( RemoteWebDriver.class ) ) ).isTrue();
+	@ParameterizedTest
+	@ValueSource( classes = { ChromeDriver.class, EdgeDriver.class, FirefoxDriver.class, InternetExplorerDriver.class,
+			OperaDriver.class, SafariDriver.class } )
+	void canCheck_should_accept_common_drivers( final Class<?> clazz ) {
+		assertThat( cut.canCheck( mock( clazz ) ) ).isTrue();
 	}
 
-	@Test
-	void seleniumAdapter_canCheck_WebElements() {
-		assertThat( cut.canCheck( mock( RemoteWebElement.class ) ) ).isTrue();
-
-		final WebElementWrapper mock = mock( WebElementWrapper.class );
-		when( mock.getWrappedElement() ).thenReturn( mock( RemoteWebElement.class ) );
+	@ParameterizedTest
+	@ValueSource( classes = { UnbreakableDriver.class, AutocheckingRecheckDriver.class, RecheckDriver.class } )
+	void canCheck_should_accept_recheck_drivers( final Class<WrapsDriver> clazz ) {
+		final WrapsDriver mock = mock( clazz );
+		when( mock.getWrappedDriver() ).thenReturn( mock( RemoteWebDriver.class ) );
 		assertThat( cut.canCheck( mock ) ).isTrue();
-
-		final WrapsElement mockedWrap = mock( WrapsElement.class );
-		when( mockedWrap.getWrappedElement() ).thenReturn( mock( RemoteWebElement.class ) );
-		assertThat( cut.canCheck( mockedWrap ) ).isTrue();
 	}
 
 	@Test
-	void seleniumAdapter_can_NOT_Check_Object() {
+	void canCheck_should_reject_Object() {
 		assertThat( cut.canCheck( new Object() ) ).isFalse();
 	}
 
 	@Test
 	void convert_invalid_should_throw_exception() {
-		assertThatThrownBy( () -> {
-			cut.convert( new Object() );
-		} ).isInstanceOf( IllegalArgumentException.class )
+		assertThatThrownBy( () -> cut.convert( new Object() ) ).isExactlyInstanceOf( IllegalArgumentException.class )
 				.hasMessageContaining( "Cannot convert objects of type 'java.lang.Object'." );
 	}
 
