a8450503b4d02d8f7f36dfe5bd72395e99bb39d3
Lars Briem <lars.briem@briemla.de>
2019-09-28 17:19:45 +0200

    Add showcase for JUnit Jupiter

---
+++ src/test/java/de/retest/web/it/ShowcaseJUnitExtensionIT.java
@@ -0,0 +1,44 @@
+package de.retest.web.it;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.WebDriver;
+
+import de.retest.recheck.Recheck;
+import de.retest.recheck.RecheckImpl;
+import de.retest.recheck.junit.RecheckExtension;
+import de.retest.web.testutils.PageFactory;
+import de.retest.web.testutils.PageFactory.Page;
+
+@ExtendWith( RecheckExtension.class )
+class ShowcaseJUnitExtensionIT {
+
+	WebDriver driver;
+	Recheck re;
+
+	@BeforeEach
+	void setUp() {
+		re = new RecheckImpl();
+	}
+
+	@ParameterizedTest( name = "showcase-{1}" )
+	@MethodSource( "de.retest.web.testutils.WebDriverFactory#drivers" )
+	void showcase_html_should_be_checked( final WebDriver driver, final String name ) throws Exception {
+		this.driver = driver;
+
+		driver.get( PageFactory.page( Page.SHOWCASE ) );
+
+		Thread.sleep( 1000 );
+
+		re.check( driver, "open" );
+	}
+
+	@AfterEach
+	void tearDown() {
+		driver.quit();
+	}
+
+}
