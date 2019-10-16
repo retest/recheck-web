ff24e14d040578ef4376739c8ab3c1b2095a8911
Daniel Kraus <daniel.kraus@retest.de>
2019-10-16 18:28:09 +0200

    Fix typo
    Tested with 13, see flag.

--- ci/script.sh
+++ ci/script.sh
@@ -13,7 +13,7 @@ export NSS_USE_SHARED_DB=ENABLED
 # Compile with JDK 8
 mvn ${MVN_ARGS} clean package -DskipTests
 
-# Test with JDK 11
+# Test with JDK 13
 wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 13
 
 if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
