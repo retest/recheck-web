8f894f00d0f0f324ba57de24228fa3adabaea736
Pascal Allion <pascal.allion@retest.de>
2019-11-14 09:59:45 +0100

    Fix javadoc

--- src/main/java/de/retest/web/ScreenshotProvider.java
+++ src/main/java/de/retest/web/ScreenshotProvider.java
@@ -8,15 +8,19 @@ import org.openqa.selenium.WebDriver;
  * The screenshot provider for way screenshots are taken.
  *
  * The ScreenshotProvider can choose whether screenshots are taken for the full page or just the viewport. The Default
- * implementation is {@link ViewPortOnlyScreenshot}.
+ * implementation is {@link ViewportOnlyScreenshot}.
  */
 public interface ScreenshotProvider {
 
 	/**
-	 * Takes the screenshot as stated.
+	 * Takes the screenshot from the {@link WebDriver}.
 	 *
-	 * @return screenshot image of the tested page.
+	 * @param driver
+	 *            Webdriver for taking the screenshot.
+	 * @return screenshot image of the tested page. May return {@code null} if screenshots are not supported.
+	 * @throws Exception
+	 *             If an exception occurred during screenshot creation.
 	 */
-	BufferedImage shoot( WebDriver driver );
+	BufferedImage shoot( WebDriver driver ) throws Exception;
 
 }
