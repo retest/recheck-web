41c97da13a17047f74b1a5db233989336ef1ff5f
Daniel Kraus <daniel.kraus@retest.de>
2020-03-27 08:12:23 +0100

    Run tests with newest JDK 14

--- ci/script.sh
+++ ci/script.sh
@@ -13,8 +13,8 @@ export NSS_USE_SHARED_DB=ENABLED
 # Compile with JDK 8
 mvn ${MVN_ARGS} clean package -DskipTests
 
-# Test with JDK 13
-wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 13
+# Test with JDK 14
+wget --quiet https://github.com/sormuras/bach/raw/master/install-jdk.sh && . ./install-jdk.sh -F 14
 
 if [ ${TRAVIS_SECURE_ENV_VARS} = "true" ]; then
     mvn ${MVN_ARGS} clean verify sonar:sonar -Pcoverage
