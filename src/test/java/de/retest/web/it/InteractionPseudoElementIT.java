4328720724eb07f47f1d9ebadd1f0f07184cd697
diba1013 <bastian.diehl@retest.de>
2020-08-29 13:12:35 +0200

    Have Test commit Golden Master

--- src/test/java/de/retest/web/it/InteractionPseudoElementIT.java
+++ src/test/java/de/retest/web/it/InteractionPseudoElementIT.java
@@ -1,12 +1,11 @@
 package de.retest.web.it;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,10 +14,11 @@ import org.openqa.selenium.WebElement;
 import de.retest.recheck.Recheck;
 import de.retest.recheck.RecheckImpl;
 import de.retest.recheck.RecheckOptions;
-import de.retest.recheck.persistence.SeparatePathsProjectLayout;
+import de.retest.recheck.junit.jupiter.RecheckExtension;
 import de.retest.web.testutils.PageFactory;
 import de.retest.web.testutils.WebDriverFactory;
 
+@ExtendWith( RecheckExtension.class )
 class InteractionPseudoElementIT {
 
 	Recheck re;
@@ -28,7 +28,6 @@ class InteractionPseudoElementIT {
 	void setUp( @TempDir final Path temp ) {
 		re = new RecheckImpl( RecheckOptions.builder() //
 				.disableReportUpload() //
-				.projectLayout( new SeparatePathsProjectLayout( temp.resolve( "state" ), temp.resolve( "reports" ) ) ) //
 				.setIgnore( "interaction-pseudo-elements.filter" ) //
 				.build() );
 
@@ -38,24 +37,15 @@ class InteractionPseudoElementIT {
 	@AfterEach
 	void tearDown() {
 		driver.quit();
-		re.cap();
 	}
 
 	@Test
 	void ghost_pseudo_elements_should_not_appear_because_of_css_changes() {
-		re.startTest();
-
 		driver.get( PageFactory.toPageUrlString( "pseudo/index.html" ) );
 
 		final WebElement withoutPseudo = driver.findElement( By.id( "link-without-pseudo" ) );
-		re.check( driver, "click" );
+		re.check( driver, "click" ); // Check before click
 		withoutPseudo.click();
 		re.check( driver, "click" ); // This should not capture any differences
-
-		assertThatThrownBy( re::capTest ).hasMessageEndingWith(
-				"2 check(s) in 'de.retest.web.it.InteractionPseudoElementIT' found the following difference(s):\n"
-						+ "Test 'ghost_pseudo_elements_should_not_appear_because_of_css_changes' has 1 difference(s) in 2 state(s):\n"
-						+ "click resulted in:\n"
-						+ "\tNo Golden Master found. First time test was run? Created new Golden Master, so don't forget to commit..." );
 	}
 }
